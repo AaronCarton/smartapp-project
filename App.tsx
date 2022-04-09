9fd2923f289e23994b13002240315e9d8079aea4
Aaron <aaron.carton@student.howest.be>
2022-04-09 04:40:33 +0200

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+export default function App() {
+  return (
+    <SafeAreaProvider>
+      <Text>Test</Text>
+      <StatusBar />
+    </SafeAreaProvider>
+  );
+}
