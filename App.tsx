e845cba5003a879c6d259d323d7adce87e1cb8b9
Aaron <aaron.carton@student.howest.be>
2022-04-21 22:43:56 +0200

    Create useDarkMode hook

--- App.tsx
+++ App.tsx
@@ -1,13 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar, StatusBarStyle } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import tw, { useAppColorScheme } from 'twrnc';
+import { DarkModeContext } from './hooks/darkmode';
 import Navigation from './navigation';
 
 export default function App() {
+  const [colorScheme, toggleColorScheme, setColorScheme] = useAppColorScheme(tw);
+
   return (
     <SafeAreaProvider>
-      <Navigation />
-      <StatusBar />
+      <DarkModeContext.Provider
+        value={{ colorScheme, toggleColorScheme, setColorScheme }}
+      >
+        <Navigation />
+      </DarkModeContext.Provider>
+      {/* If colorScheme is in light mode, make the statusbar black, otherwise make it white */}
+      <StatusBar style={colorScheme == 'light' ? 'dark' : 'light'} />
     </SafeAreaProvider>
   );
 }
---
+++ hooks/darkmode.tsx
@@ -0,0 +1,28 @@
+import { DarkTheme } from '@react-navigation/native';
+import { createContext, useContext } from 'react';
+import { Text } from 'react-native';
+import tw, { RnColorScheme, useAppColorScheme } from 'twrnc';
+
+// React Navigation custom dark theme
+const CustomDarkTheme = {
+  ...DarkTheme,
+  colors: {
+    ...DarkTheme.colors,
+    background: '#0c0c0c',
+  },
+};
+
+interface DarkModeContextType {
+  colorScheme: RnColorScheme;
+  toggleColorScheme: () => void;
+  setColorScheme: (colorScheme: RnColorScheme) => void;
+}
+
+const DarkModeContext = createContext<DarkModeContextType>(
+  {} as DarkModeContextType,
+);
+
+const useDarkMode = () => useContext(DarkModeContext);
+
+export default useDarkMode;
+export { useDarkMode, DarkModeContext, DarkModeContextType, CustomDarkTheme };
