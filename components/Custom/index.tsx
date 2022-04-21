9835a1be21bd15d3159faff8617fd1a391165651
Aaron <aaron.carton@student.howest.be>
2022-04-22 00:06:23 +0200

    Add Custom Text, View & Input components
    Custom Text, View & Input React-Native components with built-in Tailwind CSS and dark mode

---
+++ components/Custom/index.tsx
@@ -0,0 +1,65 @@
+import useDarkMode from '../../hooks/darkmode';
+import { Text as RNtext, View as RNview, TextInput as RNinput } from 'react-native';
+import { TextProps, ViewProps, TextInputProps } from 'react-native';
+import tw from 'twrnc';
+import { forwardRef } from 'react';
+
+type CustomTextProps = {
+  className?: string;
+  children?: React.ReactNode;
+  ref?: React.Ref<RNtext>;
+} & TextProps;
+
+const Text = ({ className, children, ref, ...props }: CustomTextProps): JSX.Element => {
+  const { colorScheme } = useDarkMode();
+  const defaultColor = colorScheme === 'light' ? 'text-slate-900' : 'text-neutral-50';
+  return (
+    <RNtext style={tw`${defaultColor} ${className ?? ''}`} ref={ref} {...props}>
+      {children}
+    </RNtext>
+  );
+};
+
+type CustomInputProps = {
+  className?: string;
+  children?: React.ReactNode;
+  ref?: React.Ref<RNinput>;
+} & TextInputProps;
+
+const TextInput = ({
+  className,
+  children,
+  ref,
+  ...props
+}: CustomInputProps): JSX.Element => {
+  const { colorScheme } = useDarkMode();
+  const defaultColor = colorScheme === 'light' ? '#0f172a' : '#fafafa';
+  return (
+    <RNinput
+      ref={ref}
+      style={tw`${className ?? ''}`}
+      placeholderTextColor={defaultColor}
+      {...props}
+    >
+      {children}
+    </RNinput>
+  );
+};
+
+type CustomViewProps = {
+  className?: string;
+  children?: React.ReactNode;
+  ref?: React.Ref<RNview>;
+} & ViewProps;
+
+const View = ({ className, children, ref, ...props }: CustomViewProps): JSX.Element => {
+  const { colorScheme } = useDarkMode();
+  return (
+    <RNview style={tw`${className ?? ''}`} ref={ref} {...props}>
+      {children}
+    </RNview>
+  );
+};
+
+export { Text, TextInput, View };
+export { CustomTextProps, CustomInputProps, CustomViewProps };
