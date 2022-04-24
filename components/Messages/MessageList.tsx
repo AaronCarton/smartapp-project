f35980f4a58bfe64b486540637e26c0105fc576e
Aaron <aaron.carton@student.howest.be>
2022-04-24 22:19:32 +0200

    Fix messagelist key error

--- components/Messages/MessageList.tsx
+++ components/Messages/MessageList.tsx
@@ -6,13 +6,10 @@ function MessageList({ messages }: { messages: MessageProps[] }) {
   return (
     <View className="m-3 w-full">
       {messages.map((messageProps, index) => (
-        <>
-          <MessageItem key={index} {...messageProps} />
-          <View
-            key={`divider-${index}`}
-            className="my-2.5 border-b border-gray-300 dark:border-gray-800"
-          />
-        </>
+        <View key={index}>
+          <MessageItem {...messageProps} />
+          <View className="my-2.5 border-b border-gray-300 dark:border-gray-800" />
+        </View>
       ))}
     </View>
   );
